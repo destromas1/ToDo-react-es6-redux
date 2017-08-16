b6c85659a5bd0cd748f7bb53d50aaf4d95f9387b
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2017-08-16 11:50:52 +0200

    Revert "Wrong Code Committed"

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
 
-console.log("Wrong Code");
-
 module.exports = {
   entry: './app/js/index.js',
   output: { path: __dirname, filename: 'bundle.js' },
